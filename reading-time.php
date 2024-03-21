92a0482cf213e28e2fc4f07a30cb2ff26686afa8
Dennis Claassen <declaassen@gmail.com>
2024-03-21 23:16:20 +0100

    Update reading-time.php

--- reading-time.php
+++ reading-time.php
@@ -1,5 +1,20 @@
 <?php
 
+/**
+ * Reading Time plugin.
+ *
+ * @author Dennis Claassen
+ *
+ * @wordpress-plugin
+ * Plugin Name: Reading Time
+ * Version: 0.1.0
+ * Description: Add more features related to reading time in your WordPress website.
+ * Requires at least: 6.2
+ * Requires PHP: 8.1
+ * Author: Dennis Claassen
+ * Author URI: https://www.dennisclaassen.nl/
+ */
+
 declare( strict_types=1 );
 
 require_once 'vendor/autoload.php';
