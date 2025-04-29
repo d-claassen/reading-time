060b5ff394b9474953d270d5964c856bf823c918
Dennis Claassen <declaassen@gmail.com>
2025-04-30 00:42:51 +0200

    Update reading-time.php

--- reading-time.php
+++ reading-time.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Reading Time plugin.
+ * DC23 Reading Time plugin.
  *
  * @author Dennis Claassen
  *
  * @wordpress-plugin
- * Plugin Name: Reading Time
+ * Plugin Name: DC23 Reading Time
  * Version: 0.1.0
  * Description: Add more features related to reading time in your WordPress website.
  * Requires at least: 6.2
@@ -19,15 +19,15 @@ declare( strict_types=1 );
 
 require_once 'vendor/autoload.php';
 
-if ( ! function_exists( 'reading_time_setup' ) ) :
+if ( ! function_exists( 'dc23_reading_time_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
 	 * Note that this function is hooked into the after_setup_theme hook, which runs
 	 * before the init hook.
 	 */
-	function reading_time_setup(): void {
+	function dc23_reading_time_setup(): void {
 		( new \DC23\ReadingTime\Schema_Integration() )->register();
 	}
 endif;
-add_action( 'init', 'reading_time_setup' );
+add_action( 'init', 'dc23_reading_time_setup' );
